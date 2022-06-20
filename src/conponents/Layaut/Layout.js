f9d35a677490c4b1d71c8b8b76bd3d19529105fb
Agnieszka Grosicka <a.drozdowska91@gmail.com>
2022-06-20 10:32:41 +0200

    enterin the data of the draws

---
+++ src/conponents/Layaut/Layout.js
@@ -0,0 +1,18 @@
+import './layout.css';
+
+function Layout(){
+    return(
+        <div className="mainContener">
+            <header>
+
+            </header>
+            <section>
+
+            </section>
+            <footer>
+
+            </footer>
+        </div>
+    )
+}
+export default Layout;
