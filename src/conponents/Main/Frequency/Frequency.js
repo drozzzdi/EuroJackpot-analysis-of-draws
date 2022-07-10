336867f795e450e82935ada0bd2da1a4e59d7738
Agnieszka Grosicka <a.drozdowska91@gmail.com>
2022-07-10 13:02:13 +0200

    added function showing results

--- src/conponents/Main/Frequency/Frequency.js
+++ src/conponents/Main/Frequency/Frequency.js
@@ -1,12 +1,56 @@
 import './frequency.css';
 import {useState} from 'react';
+import data from "../../../drawData";
 
 function Frequency(){
 
     const [mainNumber, setMainNumber]=useState('');
     const [jackNumber, setJackNumber]=useState('');
 
+    //change class
+    const[changeClass,setChangeClass]=useState(0);
+
+    const[suitMainNumber, setSuitNumber]=useState([0]);
+    const[suitJackNumber, setSuitJackNumber]=useState([0])
+
+    const CheckTheResult=()=>{
+        if(mainNumber>0 || jackNumber>0){
+            setChangeClass(1)
+            if(mainNumber>0){
+                let suitNumber=[];
+                for(let i=0;i<data.length-1;i++){
+                    let firstDraws=data[i].firstNumbers
+                    let idValue=data[i].id
+
+                    if(firstDraws.some(el=>el==mainNumber)){
+                        suitNumber.push(idValue);
+                    }
+            }
+            setSuitNumber(suitNumber)
+            console.log(suitMainNumber)
+           
+        }
+        let suitJack=[];
+        if(jackNumber>0){
+            for(let i=0;i<data.length-1;i++){
+                let secondDraws=data[i].secondNumbers
+                let idValue=data[i].id
+
+                if(secondDraws.some(el=>el==jackNumber)){
+                    suitJack.push(idValue);
+                }
+            }
+        }
+        setSuitJackNumber(suitJack)
+    }
+    else{
+        setChangeClass(0)
+    }
+
+}
+
     return(
+        <div className={changeClass?"active":"disactive"}>
         <div className="containerFrequency">
             <h2>Częstotliwość występowania wybranych numerów</h2>
             <h3>Analiza głównyh liczb 1-50:</h3>
@@ -34,8 +78,22 @@ function Frequency(){
                 value={jackNumber}
                 onChange={(e)=>setJackNumber(e.target.value)}
                 ></input>
+                <div className={changeClass?"noNumbers":"active"}>Nie wybrałeś żadnych liczb!</div>
             </div>
-            <button>Sprawdź</button>
+            <button
+            onClick={CheckTheResult}>Sprawdź</button>
+        </div>
+        <div className={changeClass?"results":"noresults"}>
+                <div className='analisisMainNumbers'>
+                    <div className='resultMain'> Liczba {mainNumber} występuje w: 
+                    <span>{suitMainNumber.length}</span> losowaniach </div>
+                   
+                </div>
+                 <div className='analisisJackNumbers'>
+                    <div className='resultJack'> Liczba {jackNumber} występuje w: <span>{suitJackNumber.length}</span> raz/y </div>
+                 </div>
+                 
+        </div>
         </div>
     )
 }
